5c7a0fde6f89ddd74b2f887a82da7ee043b64880
ph4m <bpedrohenrique847@gmail.com>
2025-03-18 00:12:10 +0000

    Adiciona o arquivo CartasSupertrunfoPedro.c
    Adiciona o arquivo CartasSupertrunfoPedro.c
    
    git add CartasSupertrunfoPedro.c

---
+++ CartasSupertrunfoPedro.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    // Variáveis para armazenar os dados das cidades
+    int populacao1, populacao2;
+    float area1, area2, pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
+
+    // Cadastro da primeira cidade
+    printf("Cadastro da primeira cidade:\n");
+    printf("Digite a população da cidade: ");
+    scanf (&populacao1);
+
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB da cidade (em bilhões): ");
+    scanf("%f", &pib1);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontos_turisticos1);
+
+    // Cadastro da segunda cidade
+    printf("\nCadastro da segunda cidade:\n");
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade (em bilhões): ");
+    scanf("%f", &pib2);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontos_turisticos2);
+
+    // Exibindo os dados das duas cidades
+    printf("\n--- Dados Cadastrados ---\n");
+
+    printf("\nCidade 1:\n");
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões\n", pib1);
+    printf("Pontos turísticos: %d\n", pontos_turisticos1);
+
+    printf("\nCidade 2:\n");
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões\n", pib2);
+    printf("Pontos turísticos: %d\n", pontos_turisticos2);
+
+    return 0;
+}
